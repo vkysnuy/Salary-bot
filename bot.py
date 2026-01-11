12d228120d0050aa40ddb81858ddf1b05e07373c
vkysnuy <masterozor@gmail.com>
2026-01-11 11:56:30 +0200

    Update for hosting

--- bot.py
+++ bot.py
@@ -1,6 +1,18 @@
 import telebot
 import os
 from dotenv import load_dotenv 
+from flask import Flask, request
+
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
+bot = telebot.TeleBot(os.getenv(BOT_TOKEN))
+app = Flask(__name__)
+
+
 from handlers import start
 from handlers import shifts
 from handlers import shifts_btn
@@ -9,9 +21,22 @@ from handlers import category_btn
 from handlers import plan_btn
 from handlers import penalties_btn
 
-load_dotenv()
 
-bot = telebot.TeleBot(os.getenv("TOKEN"))
+@app.route("/", methods=["GET"])
+def index():
+    return "Bot is runing", 200 
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    update = telebot.types.Update.de_json(
+        request.stream.read().decode("utf-8")
+    )
+    bot.process_new_updates([update])
+    return "ok", 200
+
+def setup_webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=f"{WEBHOOK_URL}/webhook")
 
 
 shifts_btn.register_handlers(bot)
@@ -23,4 +48,7 @@ start.register_handlers(bot)
 shifts.logic_shift(bot)
 
 
-bot.infinity_polling()
+
+if __name__ == "__main__":
+    setup_webhook()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
